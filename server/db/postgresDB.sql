c179a2e0a5773d0ec36144ee675b05b52cf7e6a2
Isaac <43230315+isaacmg00@users.noreply.github.com>
2022-05-01 23:19:34 -0700

    Update postgresDB.sql

--- server/db/postgresDB.sql
+++ server/db/postgresDB.sql
@@ -24,12 +24,33 @@ CREATE TABLE user_habits (
 );
 
 /* sample user info/habits to practice queries */
-INSERT INTO users (name, email, password) values ('isaac', 'sample@biz.com', '12345');
-INSERT INTO users (name, email, password) values ('izak', 'sample2@biz.com', 'abcde');
-INSERT INTO users (name, email, password) values ('isac', 'sample3@biz.com', 'qwerty');
+INSERT INTO users (user_name, name, email, password) values ('user1', 'isaac', 'sample@biz.com', '12345');
+INSERT INTO users (user_name, name, email, password) values ('admin', 'izak', 'sample2@biz.com', 'abcde');
+INSERT INTO users (user_name, name, email, password) values ('root', 'isac', 'sample3@biz.com', 'qwerty');
 
-INSERT INTO user_habits (user_habit) values ('clean up my room');
-                                                  
+/* # select * from users;
+               user_id                | user_name | name  |      email      | password
+--------------------------------------+-----------+-------+-----------------+----------
+ b4f1bff6-c72a-48bd-9956-298c3c8aa9ce | user1     | isaac | sample@biz.com  | 12345
+ 92cb53c1-ef16-4a49-bac2-a1f0657babb6 | admin     | izak  | sample2@biz.com | abcde
+ c8ff7337-97a7-498a-b514-3e022a15b969 | root      | isac  | sample3@biz.com | qwerty
+(3 rows)
+*/
+
+
+/* this will insert habits that are linked to user1's profile and associated with them. */
+INSERT INTO user_habits (linked_user, user_habit) values ('b4f1bff6-c72a-48bd-9956-298c3c8aa9ce','clean up my room');
+
+/* same goes for admin's profile, simply use their uuid when making the query to INSERT */
+INSERT INTO user_habits (linked_user, user_habit) values ('92cb53c1-ef16-4a49-bac2-a1f0657babb6','exercise for 1 hour daily');
+
+/* # select * from user_habits;
+ id |             linked_user              |        user_habit
+----+--------------------------------------+---------------------------
+  1 | b4f1bff6-c72a-48bd-9956-298c3c8aa9ce | clean up my room
+  2 | 92cb53c1-ef16-4a49-bac2-a1f0657babb6 | exercise for 1 hour daily
+(2 rows)
+*/
                                                  
   
 
