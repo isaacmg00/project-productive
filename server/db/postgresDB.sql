a95b84e06682426e70df2273d9be85badcfc6681
Isaac <43230315+isaacmg00@users.noreply.github.com>
2022-05-01 22:17:45 -0700

    Update postgresDB.sql

--- server/db/postgresDB.sql
+++ server/db/postgresDB.sql
@@ -1,14 +1,17 @@
+/* uuid extension */
 create extension if not exists "uuid-ossp";
 
+/* create database */
 CREATE DATABASE comp484_final;
 
 /* holds user account info */
 CREATE TABLE users (
-  user_id uuid PRIMARY KEY REFERENCES user_habits(id) DEFAULT
+  user_id uuid PRIMARY KEY DEFAULT
   uuid_generate_v4(),
   name VARCHAR(35) NOT NULL,
   email VARCHAR(100) NOT NULL,
   password VARCHAR(100) NOT NULL,
+  habit_id BIGINT REFERENCES user_habits(id),
   UNIQUE(email)
 );
 
