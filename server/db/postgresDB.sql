a2e94811a36a4760f9bdfc59b40cb225683b8b14
Isaac <43230315+isaacmg00@users.noreply.github.com>
2022-05-01 21:55:56 -0700

    Update postgresDB.sql

--- server/db/postgresDB.sql
+++ server/db/postgresDB.sql
@@ -1,11 +1,20 @@
 CREATE DATABASE comp484_final;
 
 /* holds user account info */
-CREATE TABLE users (id BIGSERIAL PRIMARY KEY NOT NULL,
-name VARCHAR(35) NOT NULL,
-email VARCHAR(100) NOT NULL,
-password VARCHAR(100) NOT NULL,
-UNIQUE (email));
+CREATE TABLE users (
+  id BIGSERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(35) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  habit_id BIGINT REFERENCES user_habits(id),
+  UNIQUE(email)
+);
+
+CREATE TABLE user_habits (
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  user_habit text
+);
+  
 
 
 
