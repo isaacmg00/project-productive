aad7a08ab12d18e7ef95289080ac69e3c4b651a8
Isaac <43230315+isaacmg00@users.noreply.github.com>
2022-05-01 23:03:19 -0700

    Update postgresDB.sql

--- server/db/postgresDB.sql
+++ server/db/postgresDB.sql
@@ -8,21 +8,29 @@ CREATE DATABASE comp484_final;
 CREATE TABLE users (
   user_id uuid PRIMARY KEY DEFAULT
   uuid_generate_v4(),
+  user_name VARCHAR(35) NOT NULL,
   name VARCHAR(35) NOT NULL,
   email VARCHAR(100) NOT NULL,
   password VARCHAR(100) NOT NULL,
-  habit_id BIGINT REFERENCES user_habits(id),
-  UNIQUE(email)
+  UNIQUE(email),
+  UNIQUE(user_name)
 );
 
 /* holds each user habit in a seperate table */
 CREATE TABLE user_habits (
   id BIGSERIAL NOT NULL PRIMARY KEY,
-  user_habit text
+  linked_user BIGINT NOT NULL REFERENCES users(useR_id)
+  user_habit TEXT NOT NULL
 );
 
 /* sample user info/habits to practice queries */
-INSERT INTO users (id, name, email, password) values ('
+INSERT INTO users (name, email, password) values ('isaac', 'sample@biz.com', '12345');
+INSERT INTO users (name, email, password) values ('izak', 'sample2@biz.com', 'abcde');
+INSERT INTO users (name, email, password) values ('isac', 'sample3@biz.com', 'qwerty');
+
+INSERT INTO user_habits (user_habit) values ('clean up my room');
+                                                  
+                                                 
   
 
 
