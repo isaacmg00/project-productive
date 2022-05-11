8f5a444a03d1e5903230fe826f223d2346cd9bde
Andres <AndresEM1524@gmail.com>
2022-05-11 11:21:42 -0700

    Majority of to do list is complete, created an assets and pages folder, and added sytling App.css for the to do list.

---
+++ client/src/components/TodoList.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = todo => {
+    //handles if todo entry is blank, filled with spaces, or brings together strings if they are seperated by large spaces.
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+  };
+
+  const removeTodo = id => {
+    const removedArr = [...todos].filter(todo => todo.id !== id);
+
+    setTodos(removedArr);
+  };
+
+  const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <>
+      <h1>Got something to do? Write it down!</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </>
+  );
+}
+
+export default TodoList;
