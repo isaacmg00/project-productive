0c295985151c2988f0293d41af6d6d6d3f240144
Isaac Garcia <isaacmg123@gmail.com>
2022-05-12 01:57:12 -0700

    simple route /test

--- server/server.js
+++ server/server.js
@@ -1,8 +1,32 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
+const { db } = require("./db");
 
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
 app.listen(port, () => {
   console.log(`server is up and listening on port ${port}`);
+});
+
+app.get("/test", async (req, res) => {
+  try {
+    console.log("hii");
+    const restaurantsRatingData = await db.query(
+      "SELECT * FROM restaurants left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id;"
+    );
+    res.status(200).json({
+      status: "success",
+      results: restaurantsRatingData.rows.length,
+      data: {
+        restaurants: restaurantsRatingData.rows,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
