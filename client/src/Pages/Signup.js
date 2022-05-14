457f2fe5b515f1c4e6c75f7524bea3b6d093d4ef
Andres <AndresEM1524@gmail.com>
2022-05-13 23:47:38 -0700

    Added signup page and created the functionality of it

--- client/src/Pages/Signup.js
+++ client/src/Pages/Signup.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import "../components/Card/Card.css";
 function Signup() {
   return (
-    <div>
-        <h1>Signup</h1>
+    <div className='signup-page'>
+       <div className="card">
+          <div className='signup'>
+          <label><b>First Name</b></label>
+          <input type="text" placeholder="Enter your first name" name="fistName" required></input>
+          <label><b>Email</b></label>
+          <input type="text" placeholder="Enter Email"required />
+          <label><b>Password</b></label>
+          <input type="text" placeholder="Make a password"required />
+  
+    <button type="submit">Create Account</button>
+          </div>
+        </div>
+      
     </div>
   )
 }
