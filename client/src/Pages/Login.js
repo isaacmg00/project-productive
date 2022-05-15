8c69a31e03fcad7fae91b85e0ef85feb1d71b1e6
Isaac Garcia <isaacmg123@gmail.com>
2022-05-15 16:31:30 -0700

    login useState

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,22 +1,65 @@
-import React from 'react'
+import { useState } from "react";
 import "../App.css";
 
-function Login() {
+const Login = ({ setAuth }) => {
+  const [inputs, setInputs] = useState({
+    email: "", //defaults the email as blank
+    password: "", //defaults the password as blank
+  });
+
+  const { email, password } = inputs;
+
+  const onChange = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value }); //changes the name of the input to the current state of the input field
+  };
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+
+    try {
+      const body = { email, password };
+      const response = await fetch(
+        "http://localhost:3001/api/v1/restaurants/login",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
+        }
+      );
+
+      const parseRes = await response.json(); //getting the token and storing it in a variable
+
+      if (parseRes.token) {
+        //if there is a token generated
+        localStorage.setItem("token", parseRes.token); //storing the token in the local storage
+        setAuth(true);
+        toast.success("login successfull!");
+      } else {
+        setAuth(false);
+        toast.error(parseRes); //returning the error created in the login route in server.js
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
   return (
-    <div className='signup-page'>
-    <div className="card">
-       <div className='signup'>
-       <label><b>Email</b></label>
-       <input type="text" placeholder="Enter Email"required />
-       <label><b>Password</b></label>
-       <input type="text" placeholder="Enter your password"required />
+    <div className="signup-page">
+      <div className="card">
+        <div className="signup">
+          <label>
+            <b>Email</b>
+          </label>
+          <input type="text" placeholder="Enter Email" required />
+          <label>
+            <b>Password</b>
+          </label>
+          <input type="text" placeholder="Enter your password" required />
 
- <button type="submit">Login</button>
-       </div>
-     </div>
-   
- </div>
-  )
-}
+          <button type="submit">Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,30 +3,7 @@ import { Link } from "react-router-dom";
 import Button from "../Button/Button";
 import "./Navbar.css";
 
-const NavBar = ({ setAuth }) => {
-  const [name, setName] = useState("");
-
-  const getProfile = async () => {
-    try {
-      const response = await fetch("http://localhost:3001/api/v1/getName/", {
-        method: "GET",
-      });
-      const parseRes = await response.json();
-      setName(parseRes.nam);
-    } catch (err) {
-      console.error(err.message);
-    }
-  };
-  const logout = async (e) => {
-    e.preventDefault();
-    localStorage.removeItem("token"); //removes the token from the local storage when a person logs out
-    setAuth(false);
-  };
-
-  useEffect(() => {
-    getProfile();
-  }, []);
-
+const NavBar = ({  }) => {
   return (
     <nav>
       <ul class="nav-list">
@@ -56,11 +33,6 @@ const NavBar = ({ setAuth }) => {
           <Link to="/signup">
             <Button />
           </Link>
-        </li>
-        <li className="nav-item">
-          <button onClick={(e) => logout(e)} className="nav-item">
-            Logout {name}
-          </button>
         </li>
       </ul>
     </nav>
