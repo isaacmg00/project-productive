b0ed1f6defc900bafb5b7b2f586c8d91b3663515
Andres <AndresEM1524@gmail.com>
2022-05-15 16:05:53 -0700

    Added buttons to habit tracker

--- client/src/Pages/HabitTracker.js
+++ client/src/Pages/HabitTracker.js
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import "../App.css";
+
 
 function HabitTracker() {
   const [backendData, setBackendData] = useState([]);
@@ -23,6 +25,10 @@ function HabitTracker() {
           backendData.habits.map((user_habit, i) => <p key={i}>{user_habit}</p>)
         )}
       </div>
+      <button type="submit">Create Habit</button>
+      <button type="submit">Edit Habit</button>
+      <button type="submit">Delete Habit</button>
+      <button type="submit">Complete</button>
     </div>
   );
 }
