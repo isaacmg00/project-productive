d7f2d8ffa7a2f68321dc96c910c5e99a0a2917bf
Sarai2028 <saraicantype@gmail.com>
2022-05-16 11:12:36 -0700

    Zenquotes

--- client/src/components/ZenQuotes.js
+++ client/src/components/ZenQuotes.js
@@ -1,18 +1,30 @@
 import React from 'react'
 
-function ZenQuotes() {
-  
-    // const api_url ="https://zenquotes.io/api/quotes/";
 
-  // ZenQuotes
-  
-    
-  // fetch(api_url)
-  // .then(response => response.json())
-  // .then(data => console.log(data));
+
+function ZenQuotes() {
+
+function getQuote() {
+  return fetch('https://zenquotes.io/api/random', {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({})
+  }).then(res => {
+      return res.json()
+  })
+    .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
+
+}
+
+
 
   return (
-    <div>ZenQuotes</div>
+    <div>ZenQuotes: 
+      <button onClick={getQuote}>get quote</button>
+    </div>
   )
 }
 
